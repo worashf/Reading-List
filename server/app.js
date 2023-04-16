41ff7f004212e25d74634d7e33d47908eef6f0b9
worashf <uppert83@gmail.com>
2023-04-16 12:12:50 +0300

    Mount graphql schema

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
-const express  = require("express")
-const  {graphqlHTTP}   = require("express-graphql")
-const app  = express()
+const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const mongoose = require("mongoose")
+const schema = require("./schema/Schema")
+const app = express()
 
+mongoose.connect('mongodb://127.0.0.1:27017/readingDB');
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
 // bind express with graphql
-app.use("/graphql", graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 
-}))
-
-
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("App started, and listening requests on port 5000")
 })
