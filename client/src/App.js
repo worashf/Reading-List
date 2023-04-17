fb12c8126cd15077dd6af17901c9cc05e9f45ddb
worashf <uppert83@gmail.com>
2023-04-17 17:47:14 +0300

    Add book list and add  book components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+
+import BookList from "./components/BooklIst";
+import AddBook from "./components/AddBook";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1> Reading list</h1>
+        <BookList />
+        <AddBook/>
+      </div>
+    </ApolloProvider>
+
   );
 }
 
